63416c05a0091b7aa57c66da8a0fa0c49014ffe3
andika rifki <andikarifki03@gmail.com>
2025-04-17 20:18:17 +0700

    add icon profile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <div class="flex justify-end mb-4 relative">
         <button @click="toggleDropdown" class="focus:outline-none">
             <div class="flex items-center">
-                <span>{{ user.username || user.name || 'Pengguna' }}</span>
+                <i class="fa-solid fa-user mr-2"></i><span>{{ user.username || user.name || 'Pengguna' }}</span>
                 <svg class="fill-current h-4 w-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path fill-rule="evenodd"
                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
